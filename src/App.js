8dd5f5d7dc4cce9fc75cee212fda79ed02bfb90d
PranavHW <154511013+PranavHW@users.noreply.github.com>
2024-10-13 20:57:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminPanel from "./components/AdminPanel/AdminPanel"; // Adjust the path accordingly
+
+function App() {
+  return (
+    <div className="App">
+      <AdminPanel />
+    </div>
+  );
+}
+
+export default App;
